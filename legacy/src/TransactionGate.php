9a36c186b447b14927db97002ae5af8d9968085a
hgsgtk <hgsgtk@gmail.com>
2019-09-29 16:41:48 +0900

    単純なコード追加をした

--- legacy/src/TransactionGate.php
+++ legacy/src/TransactionGate.php
@@ -8,13 +8,16 @@ final class TransactionGate
 
     /**
      * 各エントリに日付を設定してtransactionBundleに格納する
+     * 重複エントリチェックを追加した
      *
      * @param Entry[] $entries
      */
     public function postEntries(array $entries)
     {
         foreach ($entries as $entry) {
-            $entry->postDate();
+            if (!in_array($entry, $this->transactionBundle, true)) {
+                $entry->postDate();
+            }
         }
         $this->transactionBundle[] = $entries;
     }
